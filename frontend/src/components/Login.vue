4625acbcd4907d389e7b5ab22c16299270d15dcf
koobs97 <koobs97@naver.com>
2025-03-13 00:19:56 +0900

    20250313 로그인창 생성중_3 1. 패스워드 입력 모드 전환

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,5 +1,28 @@
 <script setup>
-import { Hide } from "@element-plus/icons-vue";
+import { Hide, View } from "@element-plus/icons-vue";
+import { reactive, computed, ref } from 'vue'
+
+/**
+ * 패스워드는 ref(반응형 변수)로 새로고침하면 사라짐
+ * state, pinia에 저장 시 XSS 공격에 취약
+ * @type {Ref<UnwrapRef<string>, UnwrapRef<string> | string>}
+ */
+const userId = ref("");
+const password = ref("");
+
+// reactive 정의
+const state = reactive({
+  isVisible: false,
+})
+
+// 패스워드 아이콘 변경
+const passwdIcon = computed(() => (state.isVisible ? View : Hide));
+const passwdType = computed(() => (state.isVisible ? "text" : "password"));
+
+// 패스워드 입력 모드 전환
+const togglePassword = () => {
+  state.isVisible = !state.isVisible;
+}
 </script>
 
 <template>
@@ -16,20 +39,24 @@ import { Hide } from "@element-plus/icons-vue";
         <el-checkbox label="로그인정보 기억하기" checked />
       </div>
       <el-input
+          v-model="userId"
           placeholder="사용자ID"
           style="
             height: 40px;
             font-size: 15px;
             margin-bottom: 8px;"/>
       <el-input
+          v-model="password"
           placeholder="비밀번호"
-          type="password"
+          :type="passwdType"
           style="
             height: 40px;
             font-size: 15px;
             margin-bottom: 16px;">
         <template #append>
-          <el-button :icon="Hide" />
+          <el-button @click="togglePassword">
+            <el-icon><component :is="passwdIcon" /></el-icon>
+          </el-button>
         </template>
       </el-input>
       <el-button
@@ -61,9 +88,7 @@ import { Hide } from "@element-plus/icons-vue";
       회원가입
     </el-button>
   </el-card>
-  <div style="text-align: right; margin-top: 12px;">
-    <img style="width: 350px; height: 51px;" src="../assets/images/logo.png" alt="Logo"  />
-  </div>
+
   <div style="
     position: fixed;
     bottom: 2%;
